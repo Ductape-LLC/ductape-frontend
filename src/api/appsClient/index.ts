c27b88966c578a6a8e50d938b2038f2ae95f594c
Opeoluwa <opesiyanbola8991@gmail.com>
2023-10-16 00:52:05 +0100

    Application varaible crud

--- src/api/appsClient/index.ts
+++ src/api/appsClient/index.ts
@@ -1,5 +1,18 @@
 import axios from "axios";
-import { APPS_BASE_URL, APPS_CREATE_URL, APPS_FETCH_URL, APP_FETCH_URL, APP_SETUP_FETCH, APP_SETUP_ENV_FETCH, APP_CREATE_CONSTANTS, APP_FETCH_CONSTANTS, APP_CREATE_VARIABLES, APP_FETCH_VARIABLES } from "./urls";
+import {
+  APPS_BASE_URL,
+  APPS_CREATE_URL,
+  APPS_FETCH_URL,
+  APP_FETCH_URL,
+  APP_SETUP_FETCH,
+  APP_SETUP_ENV_FETCH,
+  APP_CREATE_CONSTANTS,
+  APP_FETCH_CONSTANTS,
+  APP_CREATE_VARIABLES,
+  APP_FETCH_VARIABLES,
+  APP_UPDATE_VARIABLES,
+  APP_DELETE_VARIABLES,
+} from "./urls";
 import { Parameterize } from "../../utils";
 
 const source = axios.CancelToken.source();
@@ -15,10 +28,10 @@ const appsClient = (auth: string, contentType: string) => {
     baseURL: APPS_BASE_URL,
     timeout: 5000,
     headers: {
-      'Content-Type': contentType,
-      Authorization: auth
+      "Content-Type": contentType,
+      Authorization: auth,
     },
-    withCredentials: false
+    withCredentials: false,
   });
 
   // @ts-ignore
@@ -27,83 +40,150 @@ const appsClient = (auth: string, contentType: string) => {
 };
 
 type CREATE_APP = {
-  public_key: string,
-  user_id: string,
-  app_name: string
-}
+  public_key: string;
+  user_id: string;
+  app_name: string;
+};
 
 export const createApp = async (token: string, payload: CREATE_APP) => {
-  try{
-      return await appsClient(token, "application/json").post(APPS_CREATE_URL+`/?public_key=${payload.public_key}`, payload)
-  } catch(e) {
-      throw e;
-  }
-}
-
-export const fetchApps = async (token: string, data: {workspace_id: string, status: string}, public_key: string) => {
   try {
-      return await appsClient(token, "application/json").get(APPS_FETCH_URL+`/${data.workspace_id}/${data.status}/?public_key=${public_key}`)
-  } catch(e) {
-      throw e;
+    return await appsClient(token, "application/json").post(
+      APPS_CREATE_URL + `/?public_key=${payload.public_key}`,
+      payload
+    );
+  } catch (e) {
+    throw e;
   }
-}
+};
 
-export const fetchApp = async (token: string, app_id: string, user_id: string, public_key: string) => {
-  const URL = Parameterize(
-    APP_FETCH_URL,
-    ":app_id",
-    app_id
-  );
+export const fetchApps = async (
+  token: string,
+  data: { workspace_id: string; status: string },
+  public_key: string
+) => {
   try {
-      return await appsClient(token, "application/json").get(URL+`/?user_id=${user_id}&public_key=${public_key}`)
-  } catch(e) {
-      throw e;
+    return await appsClient(token, "application/json").get(
+      APPS_FETCH_URL +
+        `/${data.workspace_id}/${data.status}/?public_key=${public_key}`
+    );
+  } catch (e) {
+    throw e;
   }
-}
+};
+
+export const fetchApp = async (
+  token: string,
+  app_id: string,
+  user_id: string,
+  public_key: string
+) => {
+  const URL = Parameterize(APP_FETCH_URL, ":app_id", app_id);
+  try {
+    return await appsClient(token, "application/json").get(
+      URL + `/?user_id=${user_id}&public_key=${public_key}`
+    );
+  } catch (e) {
+    throw e;
+  }
+};
 
 export const createAppConstant = async (token: string, payload: any) => {
-  try{
-      return await appsClient(token, "application/json").post(APP_CREATE_CONSTANTS, payload)
-  } catch(e) {
-      throw e;
-  }
-}
-
-export const fetchAppConstant = async (token: string, app_id: string, user_id: string, public_key: string) => {
-  const URL = Parameterize(
-    APP_FETCH_CONSTANTS,
-    ":app_id",
-    app_id
-  );
-
-  console.log(URL, 'url')
   try {
-      return await appsClient(token, "application/json").get(URL+`/?user_id=${user_id}&public_key=${public_key}`)
-  } catch(e) {
-      throw e;
+    return await appsClient(token, "application/json").post(
+      APP_CREATE_CONSTANTS,
+      payload
+    );
+  } catch (e) {
+    throw e;
   }
-}
+};
+
+export const fetchAppConstant = async (
+  token: string,
+  app_id: string,
+  user_id: string,
+  public_key: string
+) => {
+  const URL = Parameterize(APP_FETCH_CONSTANTS, ":app_id", app_id);
+
+  console.log(URL, "url");
+  try {
+    return await appsClient(token, "application/json").get(
+      URL + `/?user_id=${user_id}&public_key=${public_key}`
+    );
+  } catch (e) {
+    throw e;
+  }
+};
 
 export const createAppVariable = async (token: string, payload: any) => {
-  try{
-      return await appsClient(token, "application/json").post(APP_CREATE_VARIABLES, payload)
-  } catch(e) {
-      throw e;
-  }
-}
-
-export const fetchAppVariable = async (token: string, app_id: string, user_id: string, public_key: string) => {
-  const URL = Parameterize(
-    APP_FETCH_VARIABLES,
-    ":app_id",
-    app_id
-  );
-
-  console.log(URL, 'url')
   try {
-      return await appsClient(token, "application/json").get(URL+`/?user_id=${user_id}&public_key=${public_key}`)
-  } catch(e) {
-      throw e;
+    return await appsClient(token, "application/json").post(
+      APP_CREATE_VARIABLES,
+      payload
+    );
+  } catch (e) {
+    throw e;
   }
-}
+};
 
+export const updateAppVariable = async (token: string, payload: any) => {
+  const {
+    variable_id,
+    user_id,
+    public_key,
+    key,
+    type,
+    description,
+    min_length,
+    max_length,
+    required,
+  } = payload;
+  const URL = Parameterize(APP_UPDATE_VARIABLES, ":variable_id", variable_id);
+
+  try {
+    return await appsClient(token, "application/json").put(
+      URL + `/?user_id=${user_id}&public_key=${public_key}`,
+      {
+        key,
+        type,
+        description,
+        min_length,
+        max_length,
+        required,
+      }
+    );
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const deleteAppVariable = async (token: string, payload: any) => {
+  const { variable_id, user_id, public_key } = payload;
+  const URL = Parameterize(APP_DELETE_VARIABLES, ":variable_id", variable_id);
+
+  try {
+    return await appsClient(token, "application/json").delete(
+      URL + `/?user_id=${user_id}&public_key=${public_key}`
+    );
+  } catch (e) {
+    throw e;
+  }
+};
+
+export const fetchAppVariable = async (
+  token: string,
+  app_id: string,
+  user_id: string,
+  public_key: string
+) => {
+  const URL = Parameterize(APP_FETCH_VARIABLES, ":app_id", app_id);
+
+  try {
+    return await appsClient(token, "application/json").get(
+      URL + `/?user_id=${user_id}&public_key=${public_key}`
+    );
+  } catch (e) {
+    throw e;
+  }
+};
