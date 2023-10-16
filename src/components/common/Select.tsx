d5f0f8a1b0126724011fa5650755a06cf2952b47
Opeoluwa <opesiyanbola8991@gmail.com>
2023-10-16 01:59:28 +0100

    update custom select component

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -1,16 +1,34 @@
-import React, { FC } from 'react';
-import { Select } from 'antd';
+import React, { FC } from "react";
+import { Select } from "antd";
 
-const ExtendedSelect: FC<any> = ({ className, options, ...inputProps }) => {
+interface CustomSelectProps {
+  options: { value: string; label: string }[];
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+const ExtendedSelect: FC<CustomSelectProps> = ({
+  options,
+  value,
+  onChange,
+  placeholder,
+  className,
+  ...props
+}) => {
   return (
     <div
       className={`w-full h-10 border rounded flex items-center ${className}`}
     >
       <Select
-        style={{ width: '100%' }}
+        style={{ width: "100%" }}
         bordered={false}
         options={options}
-        {...inputProps}
+        value={value || undefined}
+        onChange={onChange}
+        placeholder={placeholder}
+        {...props}
       />
     </div>
   );
